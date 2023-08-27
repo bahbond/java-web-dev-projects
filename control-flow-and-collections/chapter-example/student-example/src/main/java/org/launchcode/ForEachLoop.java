b6c2e4d8e5a8b504c04bbc2203b251de03a22268
bahbond <bondopicks@gmail.com>
2023-08-26 19:46:17 -0500

    Loops

--- control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/ForEachLoop.java
+++ control-flow-and-collections/chapter-example/student-example/src/main/java/org/launchcode/ForEachLoop.java
@@ -1,7 +1,7 @@
 package org.launchcode;
-import java.util.StringJoiner;
 public class ForEachLoop {
     public static void main(String[] args) {
+        //for each loop numeric
         int nums[] = {14, 3, 12, 6, 9, 4};
 
         for (int i : nums) {
@@ -9,12 +9,19 @@ public class ForEachLoop {
 
             System.out.println(i * i);
         }
-
+//foreach loop string to char array
         String message = "Tony Rice was the best!";
         char[] mess = message.toCharArray();
         for (char i : mess){
 
             System.out.println(i + " ");
+        }
+
+        //while loop
+        int l = 0;
+        while (l < 10) {
+            l++;
+            System.out.println(l);
         }
 
     }
