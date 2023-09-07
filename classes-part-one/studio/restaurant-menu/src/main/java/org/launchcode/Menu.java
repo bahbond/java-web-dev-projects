7057af58daf6e2f0b2371e30769473bcc383f00d
bahbond <bondopicks@gmail.com>
2023-09-07 14:31:40 -0500

    studio

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -4,5 +4,13 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Menu {
+private ArrayList<MenuItem> menuItems = new ArrayList<>();
+private LocalDate LastUpdated;
 
+//Allow for default constructor
+
+
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuItems;
+    }
 }
--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItem.java
@@ -1,11 +1,62 @@
 package org.launchcode;
 
+import java.time.LocalDate;
+
 public class MenuItem {
     private String name;
     private String description;
-    private Double price;
+    private double price;
     private String category;
+    private final LocalDate dateAdded;
     private boolean isNew = false;
     private boolean isAvailable = true;
 
- }
+    public MenuItem(String name, String description, double price, String category) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.isAvailable = true;
+        this.dateAdded = LocalDate.now();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public LocalDate getDateAdded() {
+        return dateAdded;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+}
